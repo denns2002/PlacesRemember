e3669a59751e4fffa2111e1a41e0a1f40eb31b9a
Denis <denis.israfilov2002@mail.ru>
2023-05-02 14:05:06 +0500

    Install hooks, separate dev and prod, create app
    Install pre-commit and hooks: black, isort and flake8 linter.
    
    Separate develop and production mods with .env.
    
    Create app memories.

--- manage.py
+++ manage.py
@@ -3,20 +3,31 @@
 import os
 import sys
 
+from dotenv import load_dotenv
+
 
 def main():
+    load_dotenv()
+
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+    if os.environ["DJANGO_DEVELOPMENT"] == "true":
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings_dev")
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
+            "Couldn't import Django. Are you sure it's installed"
+            " and "
+            "available on your PYTHONPATH environment"
+            " variable? Did you "
+            "forget to activate a virtual"
+            " environment?"
         ) from exc
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
